c0706ef55ece80f5250732bbcd6d26124f685663
NanaTake <son_magomago@yahoo.co.jp>
2019-07-02 18:28:58 +0900

    Change image-size

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   $('.form__form-for').on('submit', function(e){
     e.preventDefault();
-    console.log(this)
+    console.log(this);
     var formData = new FormData(this);
   })
-})
+})  
