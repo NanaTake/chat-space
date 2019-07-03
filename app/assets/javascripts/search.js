2d840cc1b1f5c8cc1e82d09edad05a0138a5d0ce
NanaTake <son_magomago@yahoo.co.jp>
2019-07-03 17:56:50 +0900

    Update search.js
    view search-result

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,43 +1,44 @@
 $(document).on('turbolinks:load', function(){
-  function buildHTML(data){
+  function buildHTML(datafromsearch){
     let html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">
-                    ${data.name}
+                    ${datafromsearch.name}
                   </p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${data.id}" data-user-name="${data.name}">
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${datafromsearch.id}" data-user-name="${datafromsearch.name}" >
                     追加
                   </div>
                 </div>`
+    $("#user-search-result").append(html);
   }
 
   function NoResult(message){
     let html = `<li>${message}</li>`
+    $("#user-search-result").append(html);
   }
   
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
+
     $.ajax({
       type: 'GET',
-      url: '/users/index',
+      url: '/users/search',
       data: { keyword: input },
       dataType: 'json'
     })
 
     .done(function(users) {
       $("#user-search-result").empty();
-      if (users).length !== 0) {
+      if (users.length !== 0) {
         users.forEach(function(data){
-          var html = buildHTML(data);
-          $("#user-search-result").append(html);
+          buildHTML(data);
         });
       }
       else {
-        NoResult("一致するユーザーが見つかりません");
-        $("#user-search-result").append(html);
+        NoResult("一致するユーザーが見つかりません");   
       }
     })
     .fail(function() {
-      alert('error');
+      alert('ユーザー検索に失敗しました');
     });
   });
 });
