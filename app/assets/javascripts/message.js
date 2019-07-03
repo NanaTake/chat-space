043c5458b6e43275c3e4bf9ef3ff55939fb87caa
NanaTake <son_magomago@yahoo.co.jp>
2019-07-03 11:56:10 +0900

    Update message.js
    use .reset

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -35,7 +35,7 @@ $(document).on('turbolinks:load', function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.form__form-for__message').val('');
+      $('#new_message')[0].reset();
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
     .fail(function(data){
